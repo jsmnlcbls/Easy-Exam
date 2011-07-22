40a805ae42b58cf5d40b87d8890199ad25026b1d
Jose Manuel A. Cabiles III <enilehcim@gmail.com>
2011-07-23 05:13:14 +0800

    Added new columns and a table to install

--- install.php
+++ install.php
@@ -3,19 +3,26 @@ include "/functions/common.php";
 
 $database = getDatabase();
 
-$query = "CREATE TABLE category (category_id INTEGER PRIMARY KEY, name TEXT UNIQUE, "
+$query = "CREATE TABLE IF NOT EXISTS category (category_id INTEGER PRIMARY KEY, name TEXT UNIQUE, "
 	   . "parent_category INTEGER, FOREIGN KEY (parent_category) REFERENCES "
 	   . "category(category_id))";
 $database->query($query);
 
 $query = "CREATE TABLE IF NOT EXISTS questions (question_id INTEGER PRIMARY KEY, "
 	   . "question TEXT, answer TEXT, choiceA TEXT, choiceB TEXT, choiceC TEXT, "
-	   . "choiceD TEXT, choiceE TEXT, category INTEGER, FOREIGN KEY(category) "
-	   . "REFERENCES category(category_id));";
-
+	   . "choiceD TEXT, choiceE TEXT, category INTEGER, type TEXT, "
+	   . "FOREIGN KEY(category) REFERENCES category(category_id));";
 $database->query($query);
 
 $query = "INSERT INTO category (category_id, name) VALUES (0, '');";
+$success = @$database->query($query);
+
+$query = "CREATE TABLE IF NOT EXISTS exam (exam_id INTEGER PRIMARY KEY, name TEXT, "
+	   . "start_date TEXT, end_date TEXT, questions TEXT);";
 $database->query($query);
 
-echo "Installation Done";
+if ($success) {
+	echo "Installation Done";
+} else {
+	echo "Installation Failed";
+}
