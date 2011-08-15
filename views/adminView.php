67d993fcae47d511f7b14cfd5c0a703df6cb035e
Jose Manuel A. Cabiles III <enilehcim@gmail.com>
2011-08-15 19:32:11 +0800

    Converted long if-else chain to an array lookup

--- views/adminView.php
+++ views/adminView.php
@@ -25,45 +25,46 @@
 			<li><a href="?view=selectExam">Edit</a></li>
 			<li><a href="?view=deleteExam">Delete</a></li>
 		</ul>
+		<br/>
+		<span class="submenu-title">User</span>
+		<ul class = "sub-menu">
+			<li><a href="?view=addUser">Add</a></li>
+			<li><a href="?view=listUser">List</a></li>
+		</ul>
 		
 	</div>
 	<div id = "main-panel">
 		<?php
-		if ($view == "addCategory") {
-			include "views/addCategory.php";
-		} else if ($view == "selectCategory") {
-			include "views/selectCategory.php";
-		} else if ($view == "editCategory") {
-			include "views/editCategory.php";
-		} else if ($view == "deleteCategory") {
-			include "views/deleteCategory.php";
-		}else if ($view == "addQuestion") { 
-			include "views/addQuestion.php";
-		} else if ($view == "searchQuestion") {
-			include "views/searchQuestion.php";
-		} else if ($view == "editQuestion" || $view == "editExamQuestion") {
-			include "views/editQuestion.php";
-		} else if ($view == "searchResultsQuestion") {
-			include "views/searchResultsView.php";
-		} else if ($view == "addExam") {
-			include "views/addExam.php";
-		} else if ($view == "selectExam") {
-			include "views/selectExam.php";
-		} else if ($view == "editExam") {
+		$viewFile = array(
+			'addCategory' => 'addCategory.php', 
+			'selectCategory' => 'selectCategory.php',
+			'editCategory' => 'editCategory.php',
+			'deleteCategory' => 'deleteCategory.php',
+			'addQuestion' => 'addQuestion.php',
+			'searchQuestion' => 'searchQuestion.php',
+			'editQuestion' => 'editQuestion.php',
+			'editExamQuestion' => 'editQuestion.php',
+			'searchResultsQuestion' => 'searchResultsView.php',
+			'addExam' => 'addExam.php',
+			'selectExam' => 'selectExam.php',
+			'deleteQuestion' => 'deleteQuestion.php',
+			'deleteExam' => 'deleteExam.php',
+			'addUser' => 'addUser.php'
+		);
+		
+		if ($view == "editExam") {
 			$examView = filterGet("examView");
 			if ($examView == "properties") {
 				include "views/editExam.php";
 			} else if ($examView == "questions") {
 				include "views/editExamQuestions.php";
 			}
-		} else if ($view == "deleteQuestion") { 
-			include "views/deleteQuestion.php";
-		} else if ($view == "deleteExam") {
-			include "views/deleteExam.php";
 		} else if ($view == "success") {
 			echo "<h2>Success!</h2>";
 		} else if ($view == "error") {
 			echo "<h2>Error. Please try again.</h2>";
+		} else if (isset($viewFile[$view])) {
+			include "views/" . $viewFile[$view];
 		}
 		?>
 	</div>
